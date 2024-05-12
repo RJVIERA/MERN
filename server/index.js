8898b00e003e5447f26d4cc91d4d4b0153fdb312
Satoru <rjviera10@gmail.com>
2024-05-12 11:53:24 -0700

    updated

--- server/index.js
+++ server/index.js
@@ -23,6 +23,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
+app.use(cors(
+  {
+    origin:[" "],
+    methods: ["POST", "GET"],
+    credentials:true
+  }
+));
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
