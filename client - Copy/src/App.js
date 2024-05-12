babcfe16f06cbe4dc33e484ba4b864f4ae304cb0
Satoru <rjviera10@gmail.com>
2024-05-12 12:14:14 -0700

    app

--- client - Copy/src/App.js
+++ client - Copy/src/App.js
@@ -7,11 +7,22 @@ import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
+import axios from 'axios';
 
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   const isAuth = Boolean(useSelector((state) => state.token));
+  axios.default.withCredentials = true;
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    axios.post('https://mern-i3examg27-rj-vieras-projects.vercel.app/', {email, password})
+    .then(result => console.log(result))
+    .catch(err => console.log(err)) 
+  }
+
+
+
 
   return (
     <div className="app">
