a9016de0a084d00089fd884352df650694973945
Satoru <rjviera10@gmail.com>
2024-05-14 07:33:56 -0700

    front

--- server/api/index.js
+++ server/api/index.js
@@ -25,7 +25,7 @@ dotenv.config();
 const app = express();
 app.use(cors(
   {
-    origin:["https://mern-client-eb24rkw74-rj-vieras-projects.vercel.app/"],
+    origin:["https://mern-backend-navy-six.vercel.app/"],
     methods: ["POST", "GET"],
   }
 ));
