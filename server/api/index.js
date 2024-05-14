aa3ead4afc0ce68a21e192180838709271d14fb3
Satoru <rjviera10@gmail.com>
2024-05-14 08:01:29 -0700

    a

--- server/api/index.js
+++ server/api/index.js
@@ -23,13 +23,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
-app.use(cors(
-  {
-    origin:["https://mern-front-psi.vercel.app/"],
-    methods: ["POST", "GET"],
-  }
-));
-
 app.use(express.json ())
 mongoose.connect('mongodb+srv://rjviera10:dazai2323@test.rdtzwin.mongodb.net/?retryWrites=true&w=majority&appName=Test')
 app.use(express.json());
